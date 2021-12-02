429e51947c286dcb18f5687ad15432125de0f7a5
SNazarko <sorokinnazarne@gmail.com>
2021-12-02 12:41:22 +0200

    fix

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -3,14 +3,14 @@ import 'dart:math';
 class CalculatorBrain {
   final int height;
   final int weight;
-  late double _bmi = 0;
+  late double _bmi = 19;
 
   CalculatorBrain(
     this.height,
     this.weight,
   );
   String calculateBmi() {
-    double _bmi = weight / pow(height / 100, 2);
+    _bmi = weight / pow(height / 100, 2);
     return _bmi.toStringAsFixed(1);
   }
 
@@ -30,7 +30,7 @@ class CalculatorBrain {
     } else if (_bmi > 18.5) {
       return 'You have a normal body weight. Good job!';
     } else {
-      return 'You have a lower than normal body weight. You can rat a bit more';
+      return 'You have a lower than normal body weight. You can eat a bit more';
     }
   }
 }
